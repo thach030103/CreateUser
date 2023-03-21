6a80b907e0f33dc7e553b9d03716ed197196e370
thach030103 <120116007+thach030103@users.noreply.github.com>
2023-03-21 18:50:14 +0700

    Update createUser.sql

--- createUser.sql
+++ createUser.sql
@@ -391,3 +391,91 @@ group by NGAYTD, TENSAN,clb1.TENCLB,clb2.TENCLB ,KETQUA, TRANDAU.VONG
 
 
 
+
+
+--i) Tạo và phân quyền trên Procedure:
+--1. Cho biết mã số, họ tên, ngày sinh, địa chỉ và vị trí của các cầu thủ 
+--thuộc đội bóng “SHB Đà Nẵng” có quốc tịch “Brazil”.
+
+USE QLBongDa
+GO
+CREATE PROCEDURE SPCau1
+	@tenclb NVARCHAR(100),
+	@tenqg NVARCHAR(60)
+AS
+BEGIN 
+	SELECT CT.MACT, CT.HOTEN, CT.NGAYSINH, CT.DIACHI, CT.VITRI
+	FROM CAUTHU CT,CAULACBO CLB, QUOCGIA QG
+	WHERE CT.MACLB=CLB.MACLB AND CT.MAQG=QG.MAQG AND CLB.TenCLB = @tenclb AND QG.TenQG = @tenqg
+END
+GO
+
+
+--2. Cho biết kết quả (MATRAN, NGAYTD, TENSAN, TENCLB1, TENCLB2,
+--KETQUA) các trận đấu vòng 3 của mùa bóng năm 2009.
+USE QLBongDa
+GO
+CREATE PROCEDURE SPCau2 
+	@vong INT,
+	@nam INT
+AS
+BEGIN
+	SELECT TD.MATRAN, TD.NGAYTD,SVD.TENSAN, CLB1.TENCLB AS TENCLB1, CLB2.TENCLB AS TENCLB2, TD.KETQUA
+	FROM TRANDAU TD, SANVD SVD, CAULACBO CLB1,CAULACBO CLB2
+	WHERE TD.MASAN=SVD.MASAN AND TD.MACLB1=CLB1.MACLB AND TD.MACLB2=CLB2.MACLB AND TD.VONG = @vong AND TD.NAM = @nam
+END
+GO
+
+
+
+--3. Cho biết mã huấn luyện viên, họ tên, ngày sinh, địa chỉ, 
+--vai trò và tên CLB đang làm việc của các huấn luyện viên có quốc tịch “Việt Nam”.
+USE QLBongDa
+GO
+CREATE PROCEDURE SPCau3 
+	@tenqg NVARCHAR(100)
+AS
+BEGIN
+	SELECT HLV.MAHLV,HLV.TENHLV,HLV.NGAYSINH,HLV.DIACHI,HC.VAITRO,CLB.TENCLB
+	FROM CAULACBO CLB,HUANLUYENVIEN HLV, HLV_CLB HC, QUOCGIA QG
+	WHERE CLB.MACLB=HC.MACLB AND HLV.MAHLV=HC.MAHLV AND HLV.MAQG=QG.MAQG AND QG.TENQG= @tenqg
+END
+GO
+
+
+--4. Cho biết mã câu lạc bộ, tên câu lạc bộ, tên sân vận động, địa chỉ
+--và số lượng cầu thủ nước ngoài (có quốc tịch khác “Việt Nam”) tương ứng
+--của các câu lạc bộ có nhiều hơn 2 cầu thủ nước ngoài.
+
+
+USE QLBongDa
+GO
+CREATE PROCEDURE SPCau4 
+	@tenqg NVARCHAR(100)
+AS
+BEGIN
+	SELECT CLB.MaCLB, CLB.TenCLB, SVD.TENSAN, SVD.DIACHI, COUNT(CT.MACT) as SoLuongCauThuNuocNgoai
+	FROM CAULACBO CLB,SANVD SVD,CAUTHU CT, QUOCGIA QG
+	WHERE CLB.MASAN = SVD.MASAN AND CLB.MACLB = CT.MACLB AND CT.MAQG=QG.MAQG AND QG.TENQG <> @tenqg
+	GROUP BY CLB.MACLB, CLB.TENCLB, SVD.TENSAN, SVD.DIACHI
+	HAVING COUNT(CT.MACT) > 2;
+END
+GO
+
+
+--5. Cho biết tên tỉnh, số lượng cầu thủ đang thi đấu ở vị trí tiền đạo
+--trong các câu lạc bộ thuộc địa bàn tỉnh đó quản lý.
+
+USE QLBongDa
+GO
+CREATE PROCEDURE SPCau5
+	@vitri NVARCHAR(20)
+AS
+BEGIN
+	SELECT T.TENTINH,COUNT(CT.MACT) AS SOLUONGCAUTHU
+	FROM CAUTHU CT,TINH T,CAULACBO CLB
+	WHERE CLB.MATINH=T.MATINH AND CT.MACLB=CLB.MACLB AND CT.VITRI=@vitri
+	GROUP BY T.TENTINH
+END
+GO
+
